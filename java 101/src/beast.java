4449d8da64953ff7e247c7e74b5fdedfdfa27145
ayman12390 <114825404+ayman12390@users.noreply.github.com>
2022-10-02 10:08:05 +0200

    Add files via upload

---
+++ java 101/src/beast.java
@@ -0,0 +1,22 @@
+
+
+public class beast {
+
+	
+   public int x;
+   public int y;
+  
+	public void test() 
+	{
+	   this.x = 54;
+	   
+	}
+	
+	public void any()
+	{
+		
+	}
+}
+
+  
+ 
---
+++ java 101/src/car.java
@@ -0,0 +1,16 @@
+
+  public class car {
+	
+	public int model;
+	public float price;
+	public short max_speed;
+	
+	public void speed()
+	{
+ 		
+	}
+}
+
+
+
+
